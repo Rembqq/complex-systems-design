f24bcd996c7ceba2a9e59acdf6b9eec236758239
Rembqq <nikitavolkovskyi@gmail.com>
2026-02-05 16:27:49 +0100

    initial structure

---
+++ src/main/java/org/example/lab1/Data.java
@@ -0,0 +1,55 @@
+package org.example.lab1;
+
+public class Data {
+
+    static final int N = 200;
+    static final int THREADS = 4;
+    static final int RUNS = 5;
+
+    static class Bundle {
+        int n;
+        double[][] MM, ME, MT, MZ;
+        double[] D, E;
+    }
+
+    // Calculating
+
+    // muls
+    public static double[] multiplyVecMat(double[] vec, double[][] mat) {
+
+    }
+
+    public static double[][] multiplyMatrices(double[][] mat1, double[][] mat2) {
+        return ;
+    }
+
+    public static double[][] multiplyScalMat(double scal, double[][] mat) {
+        return ;
+    }
+
+    // funcs
+    public static double[] sortVector(double[] vec) {
+
+    }
+
+    public static double[] sortVector(double[] vec) {
+
+    }
+
+    public static double[] sortMatrix(double[][] vec) {
+
+    }
+
+    public static double minVector(double[] vec) {
+        return ;
+    }
+
+    // adds subs
+    public static double[] addVector(double[] vec) {
+
+    }
+
+    public static double[][] subtractMatrices(double[][] mat1, double[][] mat2) {
+        return ;
+    }
+}
---
+++ src/main/java/org/example/lab1/FirstInstance.java
@@ -0,0 +1,15 @@
+package org.example.lab1;
+
+public class FirstInstance {
+
+    final Data.Bundle data;
+    final int threads;
+
+    public FirstInstance(Data.Bundle data, int threads) {
+        this.data = data;
+        this.threads = threads;
+    }
+
+
+
+}
